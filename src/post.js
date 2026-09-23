d652bf063ab692695b875ff11d35e801de05952b
Ajeet Dhaliwal <ajeet.dhaliwal@tesults.com>
2026-09-23 23:21:40 +0100

    Use store attachments input

--- src/post.js
+++ src/post.js
@@ -100,7 +100,7 @@ function parseArtifactUrl(output) {
 }
 
 function uploadAttachments(data) {
-  if (String(process.env.INPUT_UPLOAD_ATTACHMENTS || 'false').toLowerCase() !== 'true') {
+  if (String(process.env['INPUT_STORE-ATTACHMENTS'] || 'false').toLowerCase() !== 'true') {
     return undefined;
   }
 
